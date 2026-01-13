1350550f86f84deae7c5312c249e457c190ad2b0
Alex Epelde <alex@epelde.net>
2026-01-13 20:08:59 +0100

    ibkr: fetch historical prices
    - not foolproof: relies on query returning data for a range including
    the asked-for date.
    - not efficient: makes many queries when running `bean-price --update`

--- src/tariochbctools/plugins/prices/ibkr.py
+++ src/tariochbctools/plugins/prices/ibkr.py
@@ -1,16 +1,16 @@
 from datetime import datetime
 from os import environ
 from time import sleep
-from typing import Optional
+from typing import override
 
 from beancount.core.number import D
 from beanprice import source
 from dateutil import tz
-from ibflex import client, parser
+from ibflex import FlexQueryResponse, OpenPosition, client, parser
 
 
 class Source(source.Source):
-    def get_latest_price(self, ticker: str) -> source.SourcePrice | None:
+    def _make_query(self) -> FlexQueryResponse:
         token: str = environ["IBKR_TOKEN"]
         queryId: str = environ["IBKR_QUERY_ID"]
 
@@ -23,24 +23,50 @@ class Source(source.Source):
             else:
                 raise e
 
-        statement = parser.parse(response)
-        for custStatement in statement.FlexStatements:
-            for position in custStatement.OpenPositions:
-                symbol = position.symbol
-                symbol = symbol.rstrip("z")
-                symbol, _, _ = symbol.partition(".")
-                if symbol == ticker:
-                    price = D(position.markPrice)
-                    timezone = tz.gettz("Europe/Zurich")
-                    time = datetime.combine(
-                        position.reportDate, datetime.min.time()
-                    ).astimezone(timezone)
+        return parser.parse(response)
 
-                    return source.SourcePrice(price, time, position.currency)
+    def _handle_position(
+        self, position: OpenPosition, ticker: str, time: datetime | None = None
+    ) -> tuple[bool, source.SourcePrice | None]:
+        if (
+            position.symbol is None
+            or position.reportDate is None
+            or position.currency is None
+            or position.markPrice is None
+        ):
+            raise RuntimeError(
+                "Please adjust your FlexQuery to include the needed fields."
+            )
 
-        return None
+        symbol = position.symbol.rstrip("z")
+        symbol, _, _ = symbol.partition(".")
+        if symbol == ticker and (time is None or time.date() == position.reportDate):
+            price = D(position.markPrice)
+            timezone = tz.gettz("Europe/Amsterdam")
+            time = datetime.combine(
+                position.reportDate, datetime.min.time()
+            ).astimezone(timezone)
 
+            return True, source.SourcePrice(price, time, position.currency)
+
+        return False, None
+
+    def _get_price(
+        self, ticker: str, time: datetime | None = None
+    ) -> source.SourcePrice | None:
+        response = self._make_query()
+        for statement in response.FlexStatements[::-1]:
+            for position in statement.OpenPositions:
+                has_price, price = self._handle_position(position, ticker, time)
+                if has_price:
+                    return price
+
+    @override
+    def get_latest_price(self, ticker: str) -> source.SourcePrice | None:
+        return self._get_price(ticker)
+
+    @override
     def get_historical_price(
         self, ticker: str, time: datetime
-    ) -> Optional[source.SourcePrice]:
-        return None
+    ) -> source.SourcePrice | None:
+        return self._get_price(ticker, time)
